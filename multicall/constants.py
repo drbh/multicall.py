26fe88b65888269ebcffd2a6be35a0afd041a637
drbh <david.holtz@smartcontract.com>
2021-08-05 15:21:13 -0400

    Support more networks with Multicall deployed
    Added `BSC`, `Fantom` and `Polygon` contract addresses and enums

--- multicall/constants.py
+++ multicall/constants.py
@@ -7,6 +7,9 @@ class Network(IntEnum):
     Rinkeby = 4
     Görli = 5
     xDai = 100
+    Bsc = 56
+    Fantom = 250
+    Polygon = 137
 
 
 MULTICALL_ADDRESSES = {
@@ -15,4 +18,7 @@ MULTICALL_ADDRESSES = {
     Network.Rinkeby: '0x42Ad527de7d4e9d9d011aC45B31D8551f8Fe9821',
     Network.Görli: '0x77dCa2C955b15e9dE4dbBCf1246B4B85b651e50e',
     Network.xDai: '0xb5b692a88BDFc81ca69dcB1d924f59f0413A602a',
+    Network.Bsc: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    Network.Fantom: '0xb828c456600857abd4ed6c32facc607bd0464f4f',
+    Network.Polygon: '0x95028e5b8a734bb7e2071f96de89babe75be9c8e',
 }
